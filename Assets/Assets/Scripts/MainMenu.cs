26411cf6df09dfc2f625ef0019073738891f0e69
Jesus Morfin <jmorfin@csumb.edu>
2023-03-31 15:14:51 -0700

    Tower Defense Video 17-22

---
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad = "MainLevel";
+
+
+
+    public void Play ()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit ()
+    {
+        Debug.Log("Exciting...");
+        Application.Quit();
+    }
+}
